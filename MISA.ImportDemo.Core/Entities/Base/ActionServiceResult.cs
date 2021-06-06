667183c9c6b94a229de81680a8e0c400d12ec916
Anh Phạm <funnyanimals2k@gmail.com>
2021-06-06 22:36:36 +0700

    Add project files.

---
+++ MISA.ImportDemo.Core/Entities/Base/ActionServiceResult.cs
@@ -0,0 +1,35 @@
+﻿using MISA.ImportDemo.Core.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.ImportDemo.Core.Entities
+{
+    public class ActionServiceResult
+    {
+        public ActionServiceResult()
+        {
+        }
+
+        public ActionServiceResult(bool success, string msg, MISACode code)
+        {
+            this.Success = success;
+            this.Messenge.Add(msg);
+            this.MISACode = code;
+        }
+
+        public ActionServiceResult(bool success, string msg, MISACode code, object data)
+        {
+            this.Messenge = new List<string>();
+            this.Success = success;
+            this.Messenge.Add(msg);
+            this.MISACode = code;
+            this.Data = data;
+        }
+
+        public bool Success { get; set; } = true;
+        public List<string> Messenge { get; set; }
+        public MISACode MISACode { get; set; }
+        public object Data { get; set; }
+    }
+}
